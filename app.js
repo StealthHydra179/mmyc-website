c1a6a2973cd788966f4d66e746d131eee466c93a
Evan Qi <evanqi@gmail.com>
2020-12-25 22:09:20 -0500

    almostdone

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ const path = require('path');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
-app.use("/modules/jquery", express.static(__dirname + "/node_modules/jquery/dist"));
-app.use("/modules/font-awesome", express.static(__dirname + "/node_modules/font-awesome/css"));
-app.use("/modules/popperjs", express.static(__dirname + "/node_modules/popper.js/dist"));
-app.use("/modules/bootstrap", express.static(__dirname + "/node_modules/bootstrap/dist"));
+app.use("/modules/jquery", express.static(__dirname + "/node_modules/jquery/dist/"));
+app.use("/modules/font-awesome", express.static(__dirname + "/node_modules/font-awesome/css/"));
+app.use("/modules/popperjs", express.static(__dirname + "/node_modules/popper.js/dist/"));
+app.use("/modules/bootstrap", express.static(__dirname + "/node_modules/bootstrap/dist/"));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
@@ -26,11 +26,6 @@ app.get('/home', (req, res) => {
 app.get('/about-us', (req, res) => {
     res.locals.title = 'About Us';
     res.render('about-us');
-})
-
-app.get('/events', (req, res) => {
-    res.locals.title = 'Events';
-    res.render('events');
 })
 
 app.get('/meet-the-team', (req, res) => {
@@ -49,14 +44,50 @@ app.get('/ambassador', (req, res) => {
     res.render('ambassador');
 })
 
+app.get('/thinktank', (req, res) => {
+    res.locals.title = 'ThinkTank';
+    res.render('thinktank');
+})
+
+app.get('/collage', (req, res) => {
+    res.locals.title = 'Collage';
+    res.render('collage');
+})
+
+app.get('/frostbites', (req, res) => {
+    res.locals.title = 'Frostbites';
+    res.render('frostbites');
+})
+
+app.get('/events', (req, res) => {
+    res.locals.title = 'Our Events';
+    res.render('events');
+})
+
+app.get('/contact-us', (req, res) => {
+    res.locals.title = 'Contact Us';
+    res.render('contact-us');
+})
+
+app.get('/sponsors', (req, res) => {
+    res.locals.title = 'Sponsors';
+    res.render('sponsors');
+})
+
+app.get('/privacy', (req, res) => {
+    res.locals.title = 'Privacy Policy';
+    res.render('privacy');
+})
+
+app.get('/faq', (req, res) => {
+    res.locals.title = 'FAQ';
+    res.render('faq');
+})
+
 app.listen(3000, () => {
     console.log("Listening 3000");
 })
 
 var fs = require("fs");
-// Get content from file
 var contents = fs.readFileSync("headshots.json");
-// Define to JSON type
 var jsonContent = JSON.parse(contents);
-// Get Value from JSON
-console.log("data:", jsonContent.data);
