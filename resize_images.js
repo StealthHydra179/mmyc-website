ff0babe45d347c4e54004cd37df436ca0c5bef88
Stealth <easierlife.theemeraldforge@gmail.com>
2023-01-16 23:09:21 -0500

    Removing unneeded files

--- resize_images.js
+++ resize_images.js
@@ -5,9 +5,6 @@ console.log("Resizing Images");
 
 //images to resize
 
-// resizeImage("./public/img/logo/MMYC-Logo-1.png","./public/img/logo/resized/MMYC-Logo-210px.webp", 210, 57);
-// resizeImage("./public/img/logo/MMYC-Logo-1.png","./public/img/logo/resized/MMYC-Logo-420px.png", 420, 114);
-
 resizeImage("./public/img/events/thinktank2023/thinktank_left_banner.jpg","./public/img/events/thinktank2023/resized/thinktank_left_banner-256.webp", 256,493);
 resizeImage("./public/img/events/thinktank2023/thinktank_left_banner.jpg","./public/img/events/thinktank2023/resized/thinktank_left_banner-540.webp", 540,1041);
 resizeImage("./public/img/events/thinktank2023/thinktank_left_banner.jpg","./public/img/events/thinktank2023/resized/thinktank_left_banner-720.webp", 720,1388);
@@ -15,8 +12,6 @@ resizeImage("./public/img/events/thinktank2023/thinktank_left_banner.jpg","./pub
 resizeImage("./public/img/events/thinktank2023/thinktank_left_banner.jpg","./public/img/events/thinktank2023/resized/thinktank_left_banner-1140.webp", 1140,2198);
 resizeImage("./public/img/events/thinktank2023/thinktank_left_banner.jpg","./public/img/events/thinktank2023/resized/thinktank_left_banner-1300.webp", 1300,2506);
 resizeImage("./public/img/events/thinktank2023/thinktank_left_banner.jpg","./public/img/events/thinktank2023/resized/thinktank_left_banner-1440.webp", 1440,2776);
-
-
 
 console.log("Resizing Images Complete")
 
